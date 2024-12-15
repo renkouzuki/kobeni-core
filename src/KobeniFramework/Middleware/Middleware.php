39cd1b69d73232b0ae72c8c7d2e97dacf0116d0f
Haichhan <ppfreea1@gmail.com>
2024-12-16 04:06:11 +0700

    fixs

--- src/KobeniFramework/Middleware/Middleware.php
+++ src/KobeniFramework/Middleware/Middleware.php
@@ -2,7 +2,43 @@
 
 namespace KobeniFramework\Middleware;
 
+use KobeniFramework\Database\DB;
+use KobeniFramework\Http\Response;
+
 abstract class Middleware
 {
+    protected $db;
+
+    public function __construct()
+    {
+        if ($this->needsDatabase()) {
+            $this->db = DB::getInstance();
+        }
+    }
+
     abstract public function handle($next);
+
+    protected function needsDatabase()
+    {
+        return false;
+    }
+
+    protected function json($data, $status = 200)
+    {
+        return Response::json($data, $status);
+    }
+
+    protected function redirect($path)
+    {
+        header("Location: $path");
+        exit;
+    }
+
+    protected function getRequestData()
+    {
+        if ($_SERVER['CONTENT_TYPE'] === 'application/json') {
+            return json_decode(file_get_contents('php://input'), true);
+        }
+        return $_POST;
+    }
 }
